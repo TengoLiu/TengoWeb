74a96b2c9a6850d2eb19f75bcc918e5eb24b814e
tengo_liu <liu5087120!@163.com>
2018-09-06 09:18:02 +0800

    添加了一点注释

--- src/tengo/FirstServlet.java
+++ src/tengo/FirstServlet.java
@@ -9,16 +9,11 @@ public class FirstServlet extends GenericServlet {
 	private static final long serialVersionUID = 1L;
        
 
-    public FirstServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		OutputStream out=response.getOutputStream();
 		out.write("hello world!".getBytes());
-	
+		
 	}
 
 }
